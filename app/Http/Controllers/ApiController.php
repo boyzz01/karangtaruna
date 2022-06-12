e7aeb23c8d22ee571d13870518297c593ca06b36
Ardi Sahputra <ardysyahputra01@gmail.com>
2022-06-12 21:30:03 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -80,7 +80,7 @@ class ApiController extends Controller
 
             $saved = $temp->save();
             $tes = $no->counter+1;
-            DB::update("update counter set value = $tes where id = 1");
+          //  DB::update("update counter set value = $tes where id = 1");
             DB::commit();
             if(!$saved){
                 return response()
